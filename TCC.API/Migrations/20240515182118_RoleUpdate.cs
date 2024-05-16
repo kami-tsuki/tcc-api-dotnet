83524404fb1c33b03d6756c9741b2ef8d255e71f
Philipp Neuberg <philipp.neuberg@navaya.de>
2024-05-16 08:32:22 +0200

    migrations

---
+++ TCC.API/Migrations/20240515182118_RoleUpdate.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TCC.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoleUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RoleName",
+                table: "Roles");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoleId",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Roles",
+                type: "varchar(255)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "IsDeleted", "Name", "UpdatedAt", "UpdatedBy" },
+                values: new object[] { 1, new DateTime(2024, 5, 15, 20, 21, 17, 650, DateTimeKind.Local).AddTicks(5630), 0, false, "Guest", new DateTime(2024, 5, 15, 20, 21, 17, 650, DateTimeKind.Local).AddTicks(5701), 0 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_Name",
+                table: "Roles",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Roles_Name",
+                table: "Roles");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Roles");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoleId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true,
+                oldDefaultValue: 1);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoleName",
+                table: "Roles",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
